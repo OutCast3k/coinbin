7be22ec58484325b2a733e807c3b21bc1f226c06
Miguel Angel Nubla <ttutdxh.nubits@gmail.com>
2015-11-26 16:24:59 +0100

    Added Nu FLOT member

--- known-pubkeys.js
+++ known-pubkeys.js
@@ -9,7 +9,7 @@ var known = {
 		"0234139729dd413c84a71a0bfd6f236790be861b37311cef3240277c940e4b0c07": {name:"Nu FLOT member @jooize"},
 		"02547427fc2ea3a0ab9ef70f5e1640ff5112b113f65696948f992bd0770b942571": {name:"Nu FLOT member @woodstockmerkle"},
 		"02a144af74d018501f03d76ead130433335f969772792ec39ce389c8a234155259": {name:"Nu FLOT member @masterOfDisaster"},
-		"02e2fcdfe246e9cd4864d9119b8af465487385eccd0ea30a8cb21d44d36818189f": {name:"Nu @Dhume"},
+		"02e2fcdfe246e9cd4864d9119b8af465487385eccd0ea30a8cb21d44d36818189f": {name:"Nu FLOT member @Dhume"},
 		"034b0bd0f653d4ac0a2e9e81eb1863bb8e5743f6cb1ea40d845b939c225a1a80ff": {name:"Nu FLOT member @dysconnect"},
 		"03661a4370dfcfbcea25d1800057220f4572b6eecab95bb0670e8676a9e34451dc": {name:"Nu FLOT member @ttutdxh"},
 		"03686ee42f635c71c08f326e66139b6cb37167402cc0562584655aac03fe740495": {name:"Nu FLOT member @cryptog"},
