15eb0660b6dcfe557e7e35c022ca8f92da84f05b
Martin Kuvandzhiev <martinkuvandzhiev@gmail.com>
2017-11-10 02:27:44 +0200

    Edit for testnet.

--- js/coin.js
+++ js/coin.js
@@ -10,12 +10,10 @@
 	var coinjs = window.coinjs = function () { };
 
 	/* public vars */
-	coinjs.pub = 0x26;
-	coinjs.priv = 0x80;
-	coinjs.multisig = 0x17;
-	coinjs.hdkey = {'prv':0x0488ade4, 'pub':0x0488b21e};
-	coinjs.useForkId = false;
-	coinjs.forkId = 0;
+	coinjs.pub = 0x6f;
+	coinjs.priv = 0xef;
+	coinjs.multisig = 0xc4;
+	coinjs.hdkey = {'prv':0x04358394, 'pub':0x043587cf};
 
 	coinjs.compressed = false;
 
