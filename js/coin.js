f30b50f7ab9e48093297f0f9d04735e4f5fa8523
dabura667 <matsuringo667@gmail.com>
2017-11-18 09:43:31 +0900

    isbitcoincash into function

--- js/coin.js
+++ js/coin.js
@@ -439,6 +439,11 @@
 		return result_txt;
 	};
 
+	coinjs.isBitcoinCash = function() {
+		var host = $("#coinjs_broadcast option:selected").val();
+		return (host == 'blockdozer.com_bitcoincash')
+	}
+
 	/* start of hd functions, thanks bip32.org */
 	coinjs.hd = function(data){
 
@@ -1283,8 +1288,7 @@
 				return sequence;
 			}
 
-			var host = $("#coinjs_broadcast option:selected").val();
-			var isBitcoinCash = (host == 'blockdozer.com_bitcoincash')
+			var isBitcoinCash = coinjs.isBitcoinCash();
 
 			var shType = sigHashType || 1;
 
