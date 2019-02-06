cdf4aa105b763e527aaaaa40a3ea26566289f1b0
yottalogical <yottalogical@gmail.com>
2019-02-06 15:43:23 -0500

    Fix variable name in broadcast success message

--- js/coinbin.js
+++ js/coinbin.js
@@ -1279,7 +1279,7 @@ $(document).ready(function() {
 			},
                         success: function(data) {
 				if((data.tx) && data.tx.hash){
-					$("#rawTransactionStatus").addClass('alert-success').removeClass('alert-danger').removeClass("hidden").html(' TXID: ' + data.data.txid + '<br> <a href="https://live.blockcypher.com/btc/tx/' + data.data.txid + '" target="_blank">View on Blockchain Explorer</a>');
+					$("#rawTransactionStatus").addClass('alert-success').removeClass('alert-danger').removeClass("hidden").html(' TXID: ' + data.tx.hash + '<br> <a href="https://live.blockcypher.com/btc/tx/' + data.tx.hash + '" target="_blank">View on Blockchain Explorer</a>');
 				} else {
 					$("#rawTransactionStatus").addClass('alert-danger').removeClass('alert-success').removeClass("hidden").html(' Unexpected error, please try again').prepend('<span class="glyphicon glyphicon-exclamation-sign"></span>');
 				}
